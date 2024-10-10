f824ec78634c1f747b1432dfe68464886ead136f
Jayden Palmer <jayalexpalmer@gmail.com>
2024-10-10 09:47:24 -0500

    ParkProvider.js with functionality

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,9 @@ import Settings from './Settings.js'
 import { test } from './weather/WeatherProvider.js'
 import { GetParks } from './parks/ParkProvider.js'
 
-export const graphhopperKey = Settings.graphhopperKey
-export const npsKey = Settings.npsKey
-export const weatherKey = Settings.weatherKey
+export const {npsKey, graphhopperKey, weatherKey} = Settings
 
-GetParks()
+console.log(await GetParks())
 
 
 
--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -4,5 +4,24 @@ export const GetParks = async () => {
     const response = await fetch(`https://developer.nps.gov/api/v1/parks?limit=1&api_key=${npsKey}`)
     const parks = await response.json()
 
+    let html = '<select name="park"><option value="0">select...park</option>'
+    const parksOptions = parks.data.map(
+        (item) => {
+            return `
+            <option value="${item.id}">${item.fullName}</option>"
+            `
+        }
+    )
 
+    html += parksOptions.join("")
+    html += '</select>'
+
+    return html
+}
+
+const handleParkChoice = (changeEvent) => {
+    if (changeEvent.target.name === "park") {
+        const chosenPark = changeEvent.target.value
+        setParkChoice(chosenPark)
+    }
 }
